60b9b664f905e1996528049d3b4113c66ed18b7b
Paul Pernalon <paul.pernalon@gmail.com>
2022-02-24 21:14:08 +0100

    no https anymore

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
-import * as fs from 'fs'
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // await app.listen(3000);
-
-  const httpsOptions = {
-    key: fs.readFileSync('./../secrets/private-key.key'),
-    cert: fs.readFileSync('./../secrets/public-certificate.crt'),
-  };
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    httpsOptions,
-  });
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  await app.listen(3000)
 }
 
 
