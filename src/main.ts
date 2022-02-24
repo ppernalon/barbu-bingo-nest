97a5d2893f6912605b4faca0e7a5493e5ed4d5b2
Paul Pernalon <paul.pernalon@gmail.com>
2022-02-24 19:57:26 +0100

    self signed cert added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import fs from 'fs'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // await app.listen(3000);
+
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/ssl/private/apache-selfsigned.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/apache-selfsigned.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3000);
 }
+
+
 bootstrap();
