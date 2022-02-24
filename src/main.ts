8f18c33e024431fe55bd198f48ede85457a73159
Paul Pernalon <paul.pernalon@gmail.com>
2022-02-24 20:45:58 +0100

    fs bug

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import fs from 'fs'
+import * as fs from 'fs'
 
 async function bootstrap() {
   // const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // await app.listen(3000);
 
   const httpsOptions = {
-    key: fs.readFileSync('./secrets/private-key.key'),
-    cert: fs.readFileSync('./secrets/public-certificate.crt'),
+    key: fs.readFileSync('./../secrets/private-key.key'),
+    cert: fs.readFileSync('./../secrets/public-certificate.crt'),
   };
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     httpsOptions,
